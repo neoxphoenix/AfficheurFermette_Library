2e582b80fd9d3f92951261297770125d7286bedf
Simon Nyssen <simon.nyssen@outlook.com>
2019-01-04 10:29:52 +0100

    Documentation des méthodes associées aux vues de la DB

--- AFgestion/G_ViewEvenement.cs
+++ AFgestion/G_ViewEvenement.cs
@@ -21,20 +21,60 @@ namespace Projet_AFFICHEURFERMETTE.MDF.Gestion
          : base(sChaineConnexion)
         { }
         #endregion
+        /// <summary>
+        /// Récupère tous les événements
+        /// </summary>
+        /// <param name="Index">Trie selon l'index entré</param>
+        /// <returns>événements complets</returns>
         public List<C_ViewEvenement> Lire(string Index)
         { return new A_ViewEvenement(ChaineConnexion).Lire(Index); }
+        /// <summary>
+        /// Récupère un événement par son ID
+        /// </summary>
+        /// <param name="ID">ID de l'événement à récupérer</param>
+        /// <returns>événement complet</returns>
         public C_ViewEvenement Lire_ID(int ID)
         { return new A_ViewEvenement(ChaineConnexion).Lire_ID(ID); }
+        /// <summary>
+        /// Récupère la liste des événements commençant à la date précisée
+        /// </summary>
+        /// <param name="DateDebut">Date de début de l'événement</param>
+        /// <returns>Tous les événements commençant à la date précisée</returns>
         public List<C_ViewEvenement> Lire_DateDebut(DateTime DateDebut)
         { return new A_ViewEvenement(ChaineConnexion).Lire_DateDebut(DateDebut); }
+        /// <summary>
+        /// Récupère la liste des événements se terminant à la date précisée
+        /// </summary>
+        /// <param name="DateFin">Date de fin de l'événement</param>
+        /// <returns></returns>
         public List<C_ViewEvenement> Lire_DateFin(DateTime DateFin)
         { return new A_ViewEvenement(ChaineConnexion).Lire_DateFin(DateFin); }
+        /// <summary>
+        /// Récupère la liste des événements se déroulants à la date donnée (=> qui ont leur date de début et date de fin qui encercle la date donnée)
+        /// </summary>
+        /// <param name="Date"></param>
+        /// <returns></returns>
         public List<C_ViewEvenement> Lire_Date(DateTime Date)
         { return new A_ViewEvenement(ChaineConnexion).Lire_Date(Date); }
+        /// <summary>
+        /// Récupère les photos liées à un événement
+        /// </summary>
+        /// <param name="ID">ID de l'événement</param>
+        /// <returns>Liste des photos</returns>
         public List<C_PhotoEvenement> LirePhotosEvenement(int ID)
         { return new A_ViewEvenement(ChaineConnexion).LirePhotosEvenement(ID); }
+        /// <summary>
+        /// Récupère les personnes participant à un événement
+        /// </summary>
+        /// <param name="ID">ID de l'événement</param>
+        /// <returns>Participants</returns>
         public List<C_Personne> LirePersonnesEvenement(int ID)
         { return new A_ViewEvenement(ChaineConnexion).LirePersonnesEvenement(ID); }
+        /// <summary>
+        /// Récupère les personnes participant à une compétition, ainsi que leur position dans ladite compétition
+        /// </summary>
+        /// <param name="ID">ID de l'événement</param>
+        /// <returns>Participants et leurs positions</returns>
         public List<C_PersonnePos> LireClassementEvenement(int ID)
         { return new A_ViewEvenement(ChaineConnexion).LireClassementEvenement(ID); }
     }
--- AFgestion/G_ViewMenuDuJour.cs
+++ AFgestion/G_ViewMenuDuJour.cs
@@ -21,10 +21,25 @@ namespace Projet_AFFICHEURFERMETTE.MDF.Gestion
          : base(sChaineConnexion)
         { }
         #endregion
+        /// <summary>
+        /// Récupère tous les menus
+        /// </summary>
+        /// <param name="Index">Index de tri</param>
+        /// <returns></returns>
         public List<C_ViewMenuDuJour> Lire(string Index)
         { return new A_ViewMenuDuJour(ChaineConnexion).Lire(Index); }
+        /// <summary>
+        /// Récupère un menu par son ID
+        /// </summary>
+        /// <param name="ID">ID du menu</param>
+        /// <returns></returns>
         public C_ViewMenuDuJour Lire_ID(int ID)
         { return new A_ViewMenuDuJour(ChaineConnexion).Lire_ID(ID); }
+        /// <summary>
+        /// Récupère le menu de la date précisée
+        /// </summary>
+        /// <param name="Date">Date</param>
+        /// <returns></returns>
         public C_ViewMenuDuJour Lire_Date(DateTime Date)
         { return new A_ViewMenuDuJour(ChaineConnexion).Lire_Date(Date); }
     }
